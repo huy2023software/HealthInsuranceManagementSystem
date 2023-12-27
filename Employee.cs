f185e85a9b622a1ad793317946f8877cb018dea5
Hồ Phan Huy <131233459+huy2023software@users.noreply.github.com>
2023-12-27 23:48:49 +0700

    Complete Employee and InsuranceCompany management

---
+++ Employee.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Data.SqlTypes;
+
+namespace HealthInsuranceManagementSystem.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int EmpNo { get; set; }
+
+        [Required(ErrorMessage = "Designation can't be blank")]
+        public string Designation { get; set; }
+
+        [Required(ErrorMessage = "JoinDate can't be blank")]
+        [DisplayFormat(DataFormatString = "{0: dd/MM/yyyy}")]
+        [NotInFuture]
+        public System.DateTime JoinDate { get; set; }
+
+        [Required(ErrorMessage = "Salary can't be blank")]
+        [DisplayFormat(DataFormatString = "{0: c}")]
+        public decimal Salary { get; set; }
+
+        [Required(ErrorMessage = "First name can't be blank")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last name can't be blank")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Username can't be blank")]
+        [MinLength(8, ErrorMessage = "Username can't less than 8 character")]
+        [MaxLength(16, ErrorMessage = "Username can't exceeds than 16 character")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Password can't be blank")]
+        [MinLength(8, ErrorMessage = "Password can't less than 8 character")]
+        [MaxLength(16, ErrorMessage = "Password can't exceeds than 16 character")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Address can't be blank")]
+        public string Address { get; set; }
+
+        [Required(ErrorMessage = "Contact No can't be blank")]
+        [RegularExpression(@"^(\+|0)[0-9]+$", ErrorMessage = "Invalid phone number")]
+        public string ContactNo { get; set; }
+
+        [Required(ErrorMessage = "State can't be blank")]
+        public string State { get; set; }
+
+        [Required(ErrorMessage = "Country can't be blank")]
+        public string Country { get; set; }
+
+        [Required(ErrorMessage = "City can't be blank")]
+        public string City { get; set; }
+
+        [Required(ErrorMessage = "Policy Id can't be blank")]
+        public int PolicyId { get; set; }
+
+        [Required(ErrorMessage = "Policy Status can't be blank")]
+        public string PolicyStatus { get; set; }
+
+        public virtual Policy Policy { get; set; }
+    }
+}
---
+++ NotInFutureAttribute.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.DynamicData;
+
+namespace HealthInsuranceManagementSystem.Models
+{
+    public class NotInFutureAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value != null)
+            {
+                DateTime joinDate = (DateTime)value;
+
+                if (joinDate.Date > DateTime.Now.Date)
+                    return new ValidationResult("Join date can't be a day " +
+                        "in the future");
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
